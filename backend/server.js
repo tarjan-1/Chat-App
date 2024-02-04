38ff4302d04d7dbed983931342233b2cd50bffd6
tarjan-1 <tarjanbawankule1@gmail.com>
2024-02-05 02:39:53 +0530

    1st part completed - backend

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
+import userRoutes from './routes/user.routes.js'
+
+import connectToMongoDB from './db/connectToMongoDB.js';
+
+
+dotenv.config();
+const app = express();
+app.use(express.json()); //to parse incoming requests with json payloads
+app.use(cookieParser());
+
+app.get('/', (req, res, next) => {
+    // root route http://localhost:3000/
+    res.send('Hello world');
+})
+
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
+app.use('/api/users', userRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server is up and running on port: ${PORT}`);
+})
