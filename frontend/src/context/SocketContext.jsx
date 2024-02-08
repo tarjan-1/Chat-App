22a2438b10167642614f7f565f04822f87643d30
tarjan-1 <tarjanbawankule1@gmail.com>
2024-02-08 15:03:10 +0530

    Socket URL updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -18,7 +18,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5500", {
+      const socket = io("https://chat-app-2f0x.onrender.com", {
         query: {
           userId: authUser._id,
         },
